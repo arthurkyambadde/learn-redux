c1fce2b4b7c6654ffad376a4483418721eb12a14
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-07 15:44:09 +0300

    add reducer

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+//Action
 const CAKE_ORDERED = "CAKE_ORDERED";
 
+//Action creator
 function orderCake() {
   return {
     type: CAKE_ORDERED,
     quantity: 1,
   };
 }
+
+//State
+
+const initialState = { numberOfCakes: 10 };
+
+//Reducer
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};

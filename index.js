19f391099085d4ebfcc147c8ced7560c4faa1215
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-07 17:01:15 +0300

    create redux store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.legacy_createStore;
+
 //Action
 const CAKE_ORDERED = "CAKE_ORDERED";
 
@@ -11,7 +14,7 @@ function orderCake() {
 
 //State
 
-const initialState = { numberOfCakes: 10 };
+const initialState = { numberOfCakes: 10, anotherProperty: 5 };
 
 //Reducer
 
@@ -19,9 +22,25 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
+        ...state, //this prevents mutation of other properties(spread operator)
         numberOfCakes: state.numberOfCakes - 1,
       };
     default:
       return state;
   }
 };
+
+//Redux store
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state", store.getState());
+});
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
+store.dispatch(orderCake());
